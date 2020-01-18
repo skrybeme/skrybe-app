c62ba2280cef41eecfc1d23dac94211976054d92
eatthatpie <eatthatpie.dev@gmail.com>
2020-01-18 13:39:26 +0100

    universal picker implementation

--- src/components/UniversalPicker/index.tsx
+++ src/components/UniversalPicker/index.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import * as Styles from './styles';
+import React, { useState } from 'react';
+import * as S from './styles';
+
+function Item(props) {
+  const { title, wordsCount, modifiedDate } = props;
+
+  return (
+    <S.Item>
+      <div className="head">
+        {title}
+      </div>
+      <div className="caption">
+        <span title={`${wordsCount} words`}>
+          <i className="fas fa-skiing-nordic"></i> {wordsCount}
+        </span>
+        <span title={`Last modified at ${modifiedDate}`}>
+          <i className="far fa-clock"></i> {modifiedDate}
+        </span>
+      </div>
+    </S.Item>
+  );
+}
+
+function List() {
+  return (
+    <S.List>
+      <Item title="Blitzkrieg, Inc." wordsCount="4 321" modifiedDate="12 Sep 2019" />
+      <Item title="A Sad Story of Michael Bay" wordsCount="12 002" modifiedDate="31 Aug 2019" />
+      <Item title="Never Don't Gave Up" wordsCount="950" modifiedDate="3 May 2019" />
+      <S.Item>
+        <div className="head center">
+          See all projects...
+        </div>
+      </S.Item>
+    </S.List>
+  );
+}
 
 function UniversalPicker() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <Styles.Picker>
-      <Styles.Caption>
-        Current project
-      </Styles.Caption>
-      <Styles.Title>
-        Blitzkrieg Inc.
-      </Styles.Title>
-    </Styles.Picker>
+    <S.Context isOpen={isOpen} onClick={handleClick}>
+      <S.Picker>
+        <S.Caption>
+          Current project
+        </S.Caption>
+        <S.Title>
+          Farewell to Arms <i className="fas fa-caret-down"></i>
+        </S.Title>
+      </S.Picker>
+      <List />
+    </S.Context>
   );
 }
 
--- src/components/UniversalPicker/styles.ts
+++ src/components/UniversalPicker/styles.ts
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
+import { noSelect } from '@/styles/mixins';
 
 export const Picker = styled.div`
-  padding: 8px;
+  transition: all 0.1s ease-in-out;
+  padding: 8px 24px;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: baseline;
   flex-direction: column;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.theme.primary};
+  }
 `;
 
 export const Caption = styled.div`
@@ -15,4 +23,70 @@ export const Caption = styled.div`
 export const Title = styled.div`
   padding-top: 2px;
   font-size: 2.5rem;
+
+  i {
+    margin-left: 10px;
+  }
+`;
+
+export const List = styled.div`
+  position: absolute;
+`;
+
+export const Item = styled.div`
+  transition: all 0.1s ease-in-out;
+  background-color: ${props => props.theme.bgLight};
+  padding: 20px 24px;
+  cursor: pointer;
+
+  & + & {
+    border-top: 1px solid ${props => props.theme.bg};
+  }
+
+  &:hover {
+    background-color: ${props => props.theme.bgLightHover};
+  }
+
+  .head {
+    color: ${props => props.theme.default};
+    font-size: 2rem;
+    white-space: nowrap;
+  }
+
+  .caption {
+    color: ${props => props.theme.muted};
+    font-size: 1.6rem;
+    white-space: nowrap;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .head + .caption {
+    margin-top: 13px;
+  }
+
+  .center {
+    padding: 8px 16px;
+    text-align: center;
+  }
+`;
+
+export const Context = styled.div<any>`
+  height: 100%;
+  position: relative;
+  ${noSelect}
+
+  ${List} {
+    display: none;
+  }
+
+  ${props => props.isOpen && `
+    ${Picker} {
+      background-color: ${props.theme.primary};
+    }
+
+    ${List} {
+      display: block;
+    }
+  `}
 `;
