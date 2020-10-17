cb8a6d1fc1313352f2d23a14b02ebb1dfba77b09
eatthatpie <eatthatpie.dev@gmail.com>
2020-10-17 16:14:53 +0200

    feat: integrate generic card teaser tree view with addition buttons
    - add ButtonAddCard component to generic tree
    - draw tree lines

--- src/ui/domain-components/GenericCardTeaserTree/index.tsx
+++ src/ui/domain-components/GenericCardTeaserTree/index.tsx
@@ -3,6 +3,7 @@ import { CardTeaser } from '@/ui/components/CardTeaser';
 import { GenericCardTeaserTreeProps } from '@/interfaces/props';
 import { StoryTreeViewModel } from '@/interfaces/view-models';
 import * as S from './styles';
+import { ButtonAddCard_VariantA } from '../ButtonAddCard';
 
 export function GenericCardTeaserTree_VariantA({
   nodes
@@ -15,11 +16,19 @@ export function GenericCardTeaserTree_VariantA({
       />
       <S.LevelContext>
         {nodes?.children.map((child: StoryTreeViewModel) => (
-          <GenericCardTeaserTree_VariantA
-            key={child.id}
-            nodes={child}
-          />
+          <>
+            <S.ClickableArea key={`area-${child.id}`}>
+              <ButtonAddCard_VariantA />
+            </S.ClickableArea>
+            <GenericCardTeaserTree_VariantA
+              key={child.id}
+              nodes={child}
+            />
+          </>
         ))}
+        <S.ClickableArea isOnly={!nodes?.children?.length}>
+          <ButtonAddCard_VariantA />
+        </S.ClickableArea>
       </S.LevelContext>
     </S.GenericCardTeaserTree_VariantA>
   );
--- src/ui/domain-components/GenericCardTeaserTree/styles.ts
+++ src/ui/domain-components/GenericCardTeaserTree/styles.ts
@@ -1,10 +1,43 @@
 import { flex } from '@/ui/styles/mixins';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { CardTeaser } from '@/ui/components/CardTeaser/styles';
+import { Button } from '@/ui/components/Button/styles';
 
 export const LevelContext = styled.div`
   ${flex};
-  align-items: baseline;
+  align-items: flex-start;
+  position: relative;
+
+  > * {
+    &:before {
+      content: '';
+      background-color: lightgray;
+      height: 2px;
+      position: absolute;
+      top: -6px;
+    }
+
+    &:first-child {
+      &:before {
+        left: 50%;
+        right: 0;
+      }
+    }
+
+    &:last-child {
+      &:before {
+        left: 0;
+        right: 50%;
+      }
+    }
+
+    &:not(:first-child):not(:last-child) {
+      &:before {
+        left: 0;
+        right: 0;
+      }
+    }
+  }
 `;
 
 export const GenericCardTeaserTree_VariantA = styled.div`
@@ -13,6 +46,7 @@ export const GenericCardTeaserTree_VariantA = styled.div`
   justify-content: baseline;
   padding-top: 69px;
   position: relative;
+  z-index: 1;
 
   ${CardTeaser} {
     margin: 5px 10px;
@@ -25,4 +59,57 @@ export const GenericCardTeaserTree_VariantA = styled.div`
   ${LevelContext} & {
     padding-top: 0;
   }
+
+  &:not(:first-child) {
+    &:after {
+      background-color: lightgray;
+      content: '';
+      height: 12px;
+      left: calc(50% - 1px);
+      position: absolute;
+      right: calc(50% - 1px);
+      top: -6px;
+      z-index: -1;
+    }
+  }
+`;
+
+export const ClickableArea = styled.div<any>`
+  ${flex};
+  height: 118px;
+  position: relative;
+  width: 50px;
+  z-index: 1;
+
+  &:after {
+    border: 1px dashed lightgray;
+    bottom: 50%;
+    content: '';
+    left: calc(50% - 1px);
+    position: absolute;
+    right: calc(50% - 1px);
+    top: -6px;
+    z-index: -1;
+  }
+
+  ${Button} {
+    &:before {
+      bottom: -44px;
+      content: '';
+      left: -44px;
+      position: absolute;
+      right: -44px;
+      top: -44px;
+    }
+  }
+
+  ${props => props.isOnly && css`
+    &:before {
+      display: none;
+    }
+
+    &:after {
+      top: -12px;
+    }
+  `};
 `;
