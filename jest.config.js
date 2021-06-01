bd0dec95e52ccacf73afd46749ceef9f3811452e
eatthatpie <eatthatpie.dev@gmail.com>
2021-06-01 23:03:07 +0200

    fix: add cypress to jest ignored paths

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     "@\/(.*)$": "<rootDir>/src/$1"
   },
+  modulePathIgnorePatterns: ['<rootDir>/cypress/'],
   preset: 'ts-jest',
   setupFiles: ['./jest.setup.ts'],
   testEnvironment: 'jsdom'
