032cec94abbfe0ac2315a8e57fa4add616c6ddd7
eatthatpie <eatthatpie.dev@gmail.com>
2021-06-19 14:31:26 +0200

    fix: update theme object contract

--- src/interfaces/ITheme.ts
+++ src/interfaces/ITheme.ts
@@ -2,7 +2,14 @@ interface ITheme {
   card: {
     bg: string;
     bgHover: string;
+    bgPlaceholder: string;
+    bgPlaceholderHover: string;
     fg: string;
+    fgPlaceholder: string;
+    fgPlaceholderHover: string;
+  };
+  storyTree: {
+    edge: string;
   };
 }
 
