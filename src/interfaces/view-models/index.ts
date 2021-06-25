54ac7a564b1758beb2960484cee5854def983f3a
eatthatpie <eatthatpie.dev@gmail.com>
2021-06-25 17:46:11 +0200

    fix: missing barrel for story card view model

--- src/interfaces/view-models/index.ts
+++ src/interfaces/view-models/index.ts
@@ -1,2 +1,3 @@
+export { default as StoryCardViewModel } from './StoryCardViewModel';
 export { default as StoryTreeViewModel } from './StoryTreeViewModel';
 export { default as TagViewModel } from './TagViewModel';
