9840cf4a742734762f9bc409d7ee4e73c2111637
eatthatpie <eatthatpie.dev@gmail.com>
2021-09-28 15:35:18 +0200

    feat: placeholder for filtering tree by leaves use case

---
+++ src/use-cases/filter-story-tree-root-by-leaves/FilterStoryTreeRootByLeavesUseCase.test.ts
@@ -0,0 +1,11 @@
+describe(`FilterStoryTreeRootByLeavesUseCase`, () => {
+  describe(`when the argument is falsy`, () => {
+    it.todo(`does not change filtered collection`);
+  });
+
+  describe(`when the argument is truthy`, () => {
+    it.todo(`does not include non-leave story tree nodes in filtered collection`);
+
+    it.todo(`includes all leave story tree nodes in filtered collection`);
+  });
+});
